5e04587a8b7d1729d41504c091c9ae6750f79598
leoBG <leobg@users.noreply.github.com>
2020-05-26 20:55:38 +0200

    Interface ressource automatique

--- Assets/Scripts/UI/IntefaceRessources/InterfaceRessource.cs
+++ Assets/Scripts/UI/IntefaceRessources/InterfaceRessource.cs
@@ -1,18 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InterfaceRessource : MonoBehaviour
 {
+    private static InterfaceRessource cela;
+    public static InterfaceRessource Actuel
+    {
+        get
+        {
+            if(cela == null)
+            {
+                cela = FindObjectOfType<InterfaceRessource>();
+            }
+            return cela;
+        }
+    }
+
+    [SerializeField] private GameObject PanelInfoRessource;
+    private List<PanelInfoRessource> listePanelsInfoRessource = new List<PanelInfoRessource>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cela = this;
+        GenererPanelsInfo();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void GenererPanelsInfo()
+    {
+        for (int i = 0; i < ListeRessources.Defaut.listeDesRessources.Length; i++)
+        {
+            GameObject nvInfoRessource = Instantiate(PanelInfoRessource, transform);
+            listePanelsInfoRessource.Add(nvInfoRessource.GetComponent<PanelInfoRessource>());
+
+            listePanelsInfoRessource[i].Ressource = ListeRessources.Defaut.listeDesRessources[i];
+
+            nvInfoRessource.SetActive(true);
+        }
+
+        RearangerPanelsInfo();
+    }
+
+    private void RearangerPanelsInfo()
+    {
+        RectTransform rectPanelInfo = PanelInfoRessource.GetComponent<RectTransform>();
+        float largeurPanelInfo = rectPanelInfo.rect.width;
+
+        for (int i = 0; i < listePanelsInfoRessource.Count; i++)
+        {
+            RectTransform rectInstance = listePanelsInfoRessource[i].GetComponent<RectTransform>();
+
+            rectInstance.pivot = rectPanelInfo.pivot;
+            rectInstance.sizeDelta = rectPanelInfo.sizeDelta;
+            rectInstance.position = rectInstance.position;
+
+            rectInstance.position += new Vector3(largeurPanelInfo * i,0);
+        }
     }
 }
--- Assets/Scripts/UI/IntefaceRessources/PanelInfoRessource.cs
+++ Assets/Scripts/UI/IntefaceRessources/PanelInfoRessource.cs
@@ -1,9 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PanelInfoRessource : MonoBehaviour
 {
+    public TextMeshProUGUI stockCapacite;
+    public TextMeshProUGUI gain;
+    [SerializeField]private Image icone;
+    private Ressource ressource;
+
+    public Ressource Ressource
+    {
+        set
+        {
+            ressource = value;
+            icone.sprite = ListeIcones.Defaut.TrouverIconeRessource(ressource.nom);
+        }
+        get
+        {
+            return ressource;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
